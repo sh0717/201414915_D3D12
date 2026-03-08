aab0f1644bcd38670378eb8b24bf6d4060016a80
sh0717 <fordeer0717@naver.com>
2026-03-08 23:55:13 +0900

    claude , codex 파일 remove

--- temp_diff.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-import pathlib, sys
-sys.stdout.reconfigure(encoding='utf-8')
-with open('Docs/D3D12_Textbook/02_Part1_Ch02_RenderingMath.md', encoding='utf-8') as fh:
-    for i,line in enumerate(fh,1):
-        if '´Ù¸¨´Ï´Ù' in line:
-            print(i, line.strip())
